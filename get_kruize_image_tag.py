dc2b9e0282a0dd42596d7d5f79a10afe87dc335e
Chandrakala M Subramanyam <csubrama@redhat.com>
2023-07-20 22:12:14 +0530

    Update get_kruize_image_tag.py

--- get_kruize_image_tag.py
+++ get_kruize_image_tag.py
@@ -1,24 +1,35 @@
 import json
+import sys
 import yaml
 
-# Read YAML file
-with open('kruize-clowdapp.yaml', 'r') as f:
-    data = yaml.load(f, Loader=yaml.SafeLoader)
+def create_commitsha(yaml_file):
 
-# Write YAML object to JSON format
-with open('/tmp/kruize-clowdapp.json', 'w') as f:
-    json.dump(data, f, sort_keys=False)
+    # Read YAML file
+    with open(yaml_file, 'r') as f:
+        data = yaml.load(f, Loader=yaml.SafeLoader)
 
-# Read JSON file into Python dict
-with open('/tmp/kruize-clowdapp.json', 'r') as f:
-    json_data = json.load(f)
+    # Write YAML object to JSON format
+    with open('/tmp/kruize-clowdapp.json', 'w') as f:
+        json.dump(data, f, sort_keys=False)
 
-param = json_data["parameters"]
-for data in param:
-    if data["name"] == "KRUIZE_IMAGE_TAG":
-        commit_tag=data["value"]
-        print(data["value"])
+    # Read JSON file into Python dict
+    with open('/tmp/kruize-clowdapp.json', 'r') as f:
+        json_data = json.load(f)
 
-with open('/tmp/.commitsha', 'w') as f:
-    f.write(commit_tag)
-    f.close()
+    param = json_data["parameters"]
+    for data in param:
+        if data["name"] == "KRUIZE_IMAGE_TAG":
+            commit_tag=data["value"]
+            print(data["value"])
+
+    with open('/tmp/.commitsha', 'w') as f:
+        f.write(commit_tag)
+        f.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python get_kruize_image_tag.py <path to kruize clowdapp yaml file> ")
+        sys.exit(1)
+
+    yaml_file = sys.argv[1]
+    create_commitsha(yaml_file)
